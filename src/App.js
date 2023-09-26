ad9b92f2c2bac20b7c7f6efd47fa26032a033fee
shuklajhanvi <jhanvishukla10@gmail.com>
2023-09-26 23:40:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import  Handicraft from './component/basics/Handicraft'
+import "./component/basics/style1.css";
+
+const App = () => {
+  
+   return (<>
+  <div className='d'>
+   Indian Handicraft
+  </div>
+  <Handicraft/>
+   </>)
+};
+
+
+
+
+
+
+export default App
---
+++ src/component/basics/menuApi.js
@@ -0,0 +1,180 @@
+const Menu = [
+    
+  {
+    id: 1,
+    image: "images/IMfishc.jpg",
+    name: "Fish Form Tea light",
+    category: "Impression",
+    price: "12₹",
+    description:
+      "Bringing your decor the naturalistic positivity of conjugal fishes. Boundless liberty of the fish hand-etched in stone candle stand. The water element of the motif has been artistically neutralized with the emulation of rugged sandy hue",
+  },
+
+  {
+    id: 2,
+    image: "images/stWallImp.jpg",
+    name: "Stone Wall Lamp",
+    category: "Impression",
+    price: "20₹",
+    description:
+      "Light-up your interior with this decorative wall-light. Intricately hand- etched, the ornate depiction, raw texture & sandy pallet to render the art palpable and visual interests.",
+  },
+  {
+    id: 3,
+    image: "images/regalcandlestandimp.jpg",
+    name: "Regal Candle Stand",
+    category: "Impression",
+    price: "20₹",
+    description:
+      "A handcrafted candle stand inspired by the regal symbology of elephants and free-spirited fish from depths of the ocean. Accent your table setting with this chic representation of heritage.",
+  },
+  
+  {
+    id: 4,
+    image: "../images/GranitecandlestandIMP.jpg",
+    name: "Granite Candle Stand",
+    category: "Impression",
+    price: "50₹",
+    description:
+      "A handcrafted candle stand inspired by the regal symbology of elephants and free-spirited fish from depths of the ocean. Accent your table setting with this chic representation of heritage",
+  },
+  {
+    id: 5,
+    image: "../images/stoneplateimp.jpg",
+    name: "Stone Engraved Plate",
+    category: "Impression",
+    price: "80₹",
+    description:
+      "Serve in style with this unique platter. A stone decor with ash finish and individually hand-carved aesthetic imperfection. Bearing witness to art of bygone ages, with contemporized utility.",
+  },
+  {
+    id: 6,
+    image: "../images/table_clockImp.jpg",
+    name: "Floral Bloom Table Clock",
+    category: "Impression",
+    price: "180₹",
+    description:
+      "An intricate floral and leafy creeper motif, hand-etched to decoratively border to unordained nature of time. Furnish your setting with a unique table clock of art form that has stood the test of time. Ironic!",
+  },
+  {
+    id: 7,
+    image: "../images/eleteaset.jpg",
+    name: "Clay Tea set",
+    category: "Elementary",
+    price: "60₹",
+    description:
+      "This little tea-set is surely short & stout, but its comes out from the kiln thousand of kilometre away.Crafted by artisans who shaped it with love. All organic, all sustainable. Sophisticated in it’s own rustic way.",
+  },
+  {
+    id: 8,
+    image: "../images/naraplantrele.jpg",
+    name: "Nara Planter",
+    category: "Elementary",
+    price: "250₹",
+    description:
+      "A planter in the shape of ‘Nara’, male . With all their essentials, the turban & the moustache. ",
+  },
+  {
+    id: 9,
+    image: "../images/balaplanterexpression.jpg",
+    name: "Bala Planter",
+    category: "Elementary",
+    price: "10₹",
+    description:
+      "This figure planter, is a bundle of joy. Inspired by the women farmers of rural India, with their woven baskets, ",
+  },
+  {
+    id: 10,
+    image: "../images/guldanele.jpg",
+    name: "Guldaan Vase",
+    category: "Elementary",
+    price: "10₹",
+    description:
+      "Place it alone or with few other of its kind, put a little flower and it becomes a ‘Guldaan’",
+  },
+  {
+    id: 11,
+    image: "../images/clayjugele.jpg",
+    name: "Clay Jug sets",
+    category: "Elementary",
+    price: "10₹",
+    description:
+      "Rural India always chose the path of sustainability. This organic way of keeping water cool & fresh has been practiced for generations. its handcrafted, its ethical, its elementary & yet so beautiful. ",
+  },
+  
+    {
+      id: 12,
+      image: "../images/indoorplanter.jpg",
+      name: "Brass embossed Indoor Planter",
+      category: "Treasure",
+      price: "1250₹",
+      description:
+        "Adorn your botanical inclines with this planter box. Crafted in Brass metal, floral motif base, with curvy & ribbed tentacles to emulate the leaves. The individual and wavy leaf pattern is an attempt to represent the undulating and free-spirited rustling of the leaves.  ",
+    },
+    {
+      id: 13,
+      image: "../images/beetleleaftre.jpg",
+      name: "Beetle Leaf Plate Set",
+      category: "Treasure",
+      price: "1500₹",
+      description:
+        "An adorned serving platter tastefully served with artist’s handy pursuits. beetle-leaf motif, a cultural hallmark of the meet-greet, elaborate feast, refreshing & medicinal evergreen creeper. ",
+    },
+    {
+      id: 15,
+      image: "../images/windchimetre.jpg",
+      name: "Brass Wind Chime",
+      category: "Treasure",
+      price: "1600₹",
+      description:
+        "Bringing you the wind whisperer offering the tinkles of serenity and harmony. Inspired by the ductility of brass, melodious idiophonic triangle and the artistic conception, the wind chime makes a perfect addition to a tranquil scene-setting. ",
+    },
+    {
+      id: 16,
+      image: "../images/vasetre.jpg",
+      name: "Serpentine Vase",
+      category: "Treasure",
+      price: "5000₹",
+      description:
+        "Lend your decore a traditional inhibited floral bloom, sculpted within the serpentine grasp. A vase with handcrafted motifs bearing testimony to lores of the ages.                                                                                ",
+    },
+    {
+      id: 17,
+      image: "../images/wallLamp.jpg",
+      name: "Wall Decor Brass Lamp",
+      category: "Treasure",
+      price: "5000₹",
+      description:
+        "Style your interior with this ornate wall light. The elaborate motif bears testimony to the temple-ic  carvings, of time immemorial. Arduously hand-beaten brass transformed into conical lattice of intricacies of illumination.                   ",
+    },
+    {
+      id: 18,
+      image: "../images/embovase.jpg",
+      name: "Copper Embosed Vase",
+      category: "Treasure",
+      price: "7000₹",
+      description:
+        "Accessories your decor with this adorned copper vase,  offered with scrupulous handi-craft metal embellishments. The product boasts a traditional grand flower motif, with carvings all around. An artistically presented fusion of the art of the ages presented in an antique finish and lustrously contemporary form.",
+    },
+    {
+      id: 19,
+      image: "../images/dinnerset.jpg",
+      name: "Hand Painted Dinner set",
+      category: "Indiginious",
+      price: "7000₹",
+      description:
+    "Hosting your friends or just treating yourself, this dinner set would always set the mood right. Each piece is individually decorated & Intricately hand painted with traditional art, using all organic material. Such a unique addition to your dinnerware collection.",
+    },
+    {
+      id: 18,
+      image: "../images/drinkexp.jpg",
+      name: "Martaban Drink Expenser",
+      category: "Indiginious",
+      price: "7000₹",
+      description:
+        "This handcrafted unique clay drink dispenser, not only looks exquisite but is also utilitarian. The pores of Kutch clay keeps your drink cool while adding a dewy patrichor to it.",
+    },
+  ];
+  
+  
+  export default Menu;
